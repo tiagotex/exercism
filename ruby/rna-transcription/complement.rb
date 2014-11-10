85b54e75b29f65daec64fb10415c6d4a2ba1b332
Tiago Teixeira <tiagocmtex@gmail.com>
2014-11-10 22:07:16 +0200

    Refactor

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,21 +1,12 @@
 class Complement
-  DNA = {
-    'C' => 'G',
-    'G' => 'C',
-    'T' => 'A',
-    'A' => 'U'
-  }
+  DNA = {'C' => 'G','G' => 'C', 'T' => 'A', 'A' => 'U'}
   RNA = DNA.invert
 
   def self.of_dna(dna_strand)
-    dna_strand.each_char.map do |nucleotide|
-      DNA[nucleotide]
-    end.join
+    dna_strand.each_char.map { |nucleotide| DNA[nucleotide] }.join
   end
 
   def self.of_rna(rna_strand)
-    rna_strand.each_char.map do |nucleotide|
-      RNA[nucleotide]
-    end.join
+    rna_strand.each_char.map { |nucleotide| RNA[nucleotide] }.join
   end
 end
